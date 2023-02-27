0ad28db9be0ddf58f36a3cef989d0c2c0065ee59
vamshi1188 <saivamshik11@gmail.com>
2023-02-28 00:50:12 +0530

    added new project

---
+++ week.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class week {
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("enter the week number");
+          int week = sc.nextInt();
+          switch(week){
+            case 1 :
+            System.out.println("monday");
+            break;
+            case 2 :
+            System.out.println("tuesday");
+            break;
+            case 3 :
+            System.out.println("wednesday");
+            break;
+            case 4 :
+            System.out.println("thursday");
+            break;
+            case 5 :
+            System.out.println("friday");
+            break;
+            case 6 :
+            System.out.println("saturaday");
+            break;
+            case 7 :
+            System.out.println("sunday");
+            break;
+            default:System.out.println("there is only 7 days in a week");
+            
+            sc.close();
+          }
+    }
+    
+}
